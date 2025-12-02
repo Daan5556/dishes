b74021c97cd8d8af94af802f060f70f278b623a0
Daan Eggen <egdaan@gmail.com>
2025-12-02 15:49:51 +0100

    Added image optimizer

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,17 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
+export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+
+  eleventyConfig.addPassthroughCopy("**/favicon.svg", {
+    mode: "html-relative",
+  });
+
+  eleventyConfig.addCollection("publishedDishes", function (api) {
+    return api.getFilteredByTag("dish").filter((item) => !item.data.draft);
+  });
+}
+
 export const config = {
   dir: {
     input: "content",
